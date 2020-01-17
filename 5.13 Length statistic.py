08a3348d2216315ca72cbc979799b735d4be6a2f
Igor <unlike041986@gmail.com>
2020-01-18 00:35:52 +0200

    pocket

--- 5.13 Length statistic.py
+++ 5.13 Length statistic.py
@@ -1,6 +1,11 @@
 strName = input().split(" ")
-dct = {}
+lst = []
 for i in strName:
-    print(len(i), end=" ")
-print()
-print(strName)
+	if len(i) != 0:
+	 	lst.append(len(i))
+	 	
+#print(lst)
+
+for i in lst:
+#	print(i)
+	print(i ,  lst.count(i))
