b9b0ed304747d0c38c0a44c75d10a5108f477026
Anonimysick <unlike041986@gmail.com>
2020-01-20 17:11:48 +0200

    Home

--- 5.13 Length statistic.py
+++ 5.13 Length statistic.py
@@ -1,11 +1,13 @@
 strName = input().split(" ")
 lst = []
 for i in strName:
-	if len(i) != 0:
-	 	lst.append(len(i))
-	 	
-#print(lst)
+    if len(i) != 0:
+        lst.append(len(i))
 
-for i in lst:
-#	print(i)
-	print(i ,  lst.count(i))
+# print(lst)
+lst.sort()
+for i in range(0, len(lst)):
+    # print(lst[i])
+    print(i, i+1)
+    if lst[i] != lst[i+1]:
+        print(lst[i], lst.count(lst[i]))
